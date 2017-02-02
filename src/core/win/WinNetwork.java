cc54d8c3eb22d350c92110bc634b449930b457e7
Will Barden <wfbarden@gmail.com>
2017-02-02 09:24:29 -0500

    DOS commands in comments now

--- src/core/win/WinNetwork.java
+++ src/core/win/WinNetwork.java
@@ -12,36 +12,45 @@ import core.NetworkCore;
 
 public class WinNetwork implements NetworkCore {
 
+    //ipconfig | find "Connection-specific DNS Suffix"
     @Override
     public String getDnsSuffix() {
         return "ovh.oakland.edu";
     }
     
+    //ipconfig /all | find "DNS Servers"
     @Override
     public String getDnsIP() {
         return "";
     }
 
+    //ipconfig /all | find "IPv4 Address"
     @Override
     public String getIP() {
         return "141.210.237.178";
     }
 
+    //ipconfig | find "Subnet Mask"
     @Override
     public String getSubnetMask() {
         return "255.255.254.0";
     }
 
+    //ipconfig | find "Default Gateway"
     @Override
     public String getDefGateway() {
         return "141.210.236.1";
     }
 
+    /*ipconfig /all | find "Physical Address"
+     *will be first one listed in output
+     */
     @Override
     public String getMAC() {
         return "28-D2-44-4F-D4-12";
     }
 
+    //ping -n 1 www.google.com
     @Override
     public String pingGoogleTime() {
         return "";
