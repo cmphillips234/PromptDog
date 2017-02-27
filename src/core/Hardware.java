8d51afeb11d2c2b3259acb33a71dfa1a3dd0fe1f
cmphillips234 <caleb@zsalon.biz>
2017-02-27 16:11:25 -0500

    Mac implementation of hardware core
    I am going to need some help making sure that these will still work in Linux, but I'm using bash scripts so I feel like the commands used should translate.
    
    Note the discrepancy with BIOS. Mac doesn't use BIOS, does Linux? if so we might have a problem.

--- src/core/Hardware.java
+++ src/core/Hardware.java
@@ -33,43 +33,79 @@ public class Hardware implements HardwareCore {
     //systeminfo | find "Host Name"
     @Override
     public String getHostName() {
-        return "Will-Lenovo";
+        //return "Will-Lenovo";
+        if(OSTYPE.equals("cmd")){
+            test.GoldenRetriever();
+        }else{
+            test.GoldenRetriever("hostNameFinder", "bash", "-c", "hostname");
+        }
     }
 
     //systeminfo | find "OS Name"
     @Override
     public String getOsName() {
-        return "Microsoft Windows 10 Home";
+        //return "Microsoft Windows 10 Home";
+        return sysOS;
     }
 
     //systeminfo | find "OS Version"
     @Override
     public String getOsVersion() {
-        return "10.0.14393 N/A Build 14393";
+        //return "10.0.14393 N/A Build 14393";
+        if(OSTYPE.equals("cmd")){
+            test.GoldenRetriever();
+        }else{
+            test.GoldenRetriever("OsVersion", "bash", "-c", "sw_vers");
+        }
     }
 
     //systeminfo | find "System Type"
     @Override
     public String getSysType() {
-        return "x64-based PC";
+        //return "x64-based PC";
+        if(OSTYPE.equals("cmd")){
+            test.GoldenRetriever();
+        }else{
+            test.GoldenRetriever("SystemType", "bash", "-c", "uname -m");
+        }
     }
 
     //systeminfo | find "BIOS Version"
     @Override
     public String getBiosVersion() {
-        return "LENOVO 74CN47WW(V3.08), 1/30/2015";
+        //return "LENOVO 74CN47WW(V3.08), 1/30/2015";
+        if(OSTYPE.equals("cmd")){
+            test.GoldenRetriever();
+        }else{
+            System.out.println("not applicable to Mac/Linux based systems.);
+                               //we need to talk about ideas here. Maybe we can give EFI information?
+        }
     }
 
     //systeminfo | find "Total Physical Memory"
     @Override
     public String getTotalPhysicalMem() {
-        return "8,108 MB";
+       // return "8,108 MB";
+        
+        if(OSTYPE.equals("cmd")){
+            test.GoldenRetriever();
+        }else{
+            test.GoldenRetriever("TotalMem", "bash", "-c", "sysctl -a | grep hw.memsize:");
+            //Returns total memory in KB
+        }
+        
     }
 
     //systeminfo | find "Available Physical Memory"
     @Override
     public String getTotalAvailableMem() {
-        return "4,177 MB";
+        //return "4,177 MB";
+        if(OSTYPE.equals("cmd")){
+            test.GoldenRetriever();
+        }else{
+            test.GoldenRetriever("AvailableMem", "bash", "-c", "sysctl -a | grep hw.physmem");
+            //Returns total memory in KB
+        }
     }
 
 }
